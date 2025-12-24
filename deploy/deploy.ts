374ca99d7cec6d66fdd6cd3d7a066c79d1bbe217
andrewhsud <dyamfiqzm84901@outlook.com>
2025-12-24 15:45:22 +0800

    deploy: wire ObscureGallery deployment script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedGallery = await deploy("ObscureGallery", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ObscureGallery contract: `, deployedGallery.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_obscureGallery"; // id required to prevent reexecution
+func.tags = ["ObscureGallery"];
